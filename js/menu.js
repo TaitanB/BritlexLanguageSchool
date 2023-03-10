dbb4043bccb5041e2138d7439805ac35b18fa676
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-03-10 22:56:13 +0200

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,20 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-    body: document.querySelector('body'),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
     // menuList: document.querySelector('.mob-menu-list'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
   // refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle("is-hidden");
+    // refs.openMenuBtn.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
   // function removeMenu() {
   //   refs.menu.classList.add('is-hidden');
