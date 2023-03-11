da428473dfac3003d8382799c4faad49150abf44
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-03-11 23:09:11 +0200

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,27 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
-  };
+const refs = {
+  openMenuBtn: document.querySelector("[data-menu-open]"),
+  closeMenuBtn: document.querySelector("[data-menu-close]"),
+  menu: document.querySelector("[data-menu]"),
+  body: document.querySelector("body"),
+  wraper: document.querySelector(".nav__wraper"),
+};
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+refs.openMenuBtn.addEventListener("click", toggleMenu);
+refs.openMenuBtn.addEventListener("click", animationOpen);
+refs.closeMenuBtn.addEventListener("click", animationClose);
+refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
-})();
+function toggleMenu() {
+  refs.menu.classList.toggle("is-hidden");
+  refs.body.classList.toggle("no-scroll");
+}
+
+function animationOpen() {
+  refs.wraper.classList.add("animationOpen");
+  refs.wraper.classList.remove("animationClose");
+}
+
+function animationClose() {
+  refs.wraper.classList.add("animationClose");
+  refs.wraper.classList.remove("animationOpen");
+}
